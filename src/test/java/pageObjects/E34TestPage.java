8098a8cb29febf0179f0b81e11b1ae4596955dd4
mpalotas <michael.palotas@element34.net>
2016-10-03 21:44:25 +0200

    removed unneeded files

---
+++ src/test/java/pageObjects/E34TestPage.java
@@ -0,0 +1,46 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * Created by gridfusion on 07/05/16.
+ */
+public class NzzLoginPage {
+
+    @FindBy(id="firstname")
+    private WebElement firstName;
+
+    @FindBy(id="lastname")
+    private WebElement lastName;
+
+    @FindBy(name="submitbutton")
+    private WebElement button;
+
+
+    public NzzLoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterFirstName() {
+        firstName.sendKeys("michael");
+    }
+
+    public void enterLastName() {
+        lastName.sendKeys("palotas");
+    }
+
+    public void clickButton() {
+        button.click();
+    }
+
+    public void login() {
+        enterFirstName();
+        enterLastName();
+        clickButton();
+    }
+
+
+}
