b7bc59674a3784892e607e5703af0b3a92dad5fa
mpalotas <michael.palotas@element34.net>
2017-06-06 07:05:02 +0200

    adding more browsers

--- src/test/java/pageObjects/PostTests.java
+++ src/test/java/pageObjects/PostTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -15,6 +16,18 @@ import java.net.URL;
  * Created by gridfusion on 07/05/16.
  */
 public class PostTests {
+
+    @DataProvider(name = "platforms", parallel = true)
+    public Object[][] getCapabilities() {
+
+        return new Object[][]{
+                {DesiredCapabilities.firefox()},
+                {DesiredCapabilities.chrome()},
+                {DesiredCapabilities.edge()},
+                {DesiredCapabilities.internetExplorer()}
+        };
+    }
+
 
     @BeforeTest
     public void setup() {
@@ -37,16 +50,16 @@ public class PostTests {
         }
     }
 
-    @Test(invocationCount = 1, threadPoolSize = 5)
-    public void loginTest() throws InterruptedException, MalformedURLException {
+    @Test(dataProvider = "platforms", invocationCount = 1, threadPoolSize = 5)
+    public void loginTest(DesiredCapabilities caps) throws InterruptedException, MalformedURLException {
 
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setBrowserName("MicrosoftEdge");
+        //caps.setBrowserName("MicrosoftEdge");
         //caps.setBrowserName("firefox");
         //caps.setBrowserName("chrome");
         //caps.setVersion("50");
         //WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), caps);
         WebDriver driver = new RemoteWebDriver(new URL("https://789b1ea7eca7.element34.net/wd/hub"), caps);
+        driver.manage().window().maximize();
 
         PostHomepage homepage = new PostHomepage(driver);
         homepage.openHomePage(driver);
